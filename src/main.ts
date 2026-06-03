d0fd3801ce2e3a3aba5d6ece21573ef913fc57d7
Seth Hoong <hoongseth@gmail.com>
2026-06-03 16:22:58 +0000

    Initialize project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-function hello() {
-  console.log("Hello world!");
-}
+import express from "express";
 
-hello();
+const port = 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/public/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React, { StrictMode } from "react";
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById("app") as HTMLElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
