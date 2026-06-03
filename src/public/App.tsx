a406d3f1fee693110c49a0120dea779765b1a08b
Seth Hoong <hoongseth@gmail.com>
2026-06-03 20:13:07 +0000

    Scaffold layout structure

--- src/public/App.tsx
+++ src/public/App.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function App() {
-  return <h1>PixieDraft</h1>;
+  return (
+    <div className="three-column-layout">
+      <div id="library">
+        <button className="story-card">
+          <h2>Story 1</h2>
+        </button>
+        <button className="story-card">
+          <h2>Story 2</h2>
+        </button>
+      </div>
+      <div id="editor">
+        <input type="text" id="story-title" placeholder="Story Title" />
+        <textarea id="story-content" placeholder="Write your story here..." />
+      </div>
+    </div>
+  );
 }
