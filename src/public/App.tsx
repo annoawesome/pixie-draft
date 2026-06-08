c081a202ebfe42cbd4615cb91e4d19b2fc4164b7
Seth Hoong <hoongseth@gmail.com>
2026-06-08 01:46:45 +0000

    Refactor App component

--- src/public/App.tsx
+++ src/public/App.tsx
@@ -1,35 +1,14 @@
 import React, { useEffect, useState } from "react";
-
-import { getStories } from "./api/storiesApi";
-import Story from "./type/storyType";
-import Library from "./component/Library";
-import Editor from "./component/Editor";
-import AsideSettings from "./component/AsideSettings";
 import AuthenticatePrompt from "./component/AuthenticatePrompt";
+import MainLayout from "./component/MainLayout";
 import { refreshTokens } from "./api/authApi";
 
 export default function App() {
-  const [stories, setStories] = useState<Story[]>([]);
-  const [selectedStory, setSelectedStory] = useState<Story | null>(null);
-
   // Makes development a little easier with vite's dev server
   // Only temporary, will probably be replaced with something better
   const [apiToken, setApiToken] = useState<string>(
     window.location.host === "localhost:5173" ? "DUMMY_TOKEN" : "",
   );
-
-  useEffect(() => {
-    getStories(apiToken)
-      .then((stories) => {
-        if (stories) {
-          setStories(stories);
-        }
-      })
-      .catch((error) => {
-        // probably failed because unauthroized
-        console.error("Error fetching stories:", error);
-      });
-  }, [apiToken]);
 
   // Refresh api token every 12 minutes, 3 minutes before expiration
   useEffect(() => {
@@ -48,32 +27,9 @@ export default function App() {
     return () => clearTimeout(timeoutId);
   }, [apiToken]);
 
-  if (apiToken) {
-    return (
-      <div className="three-column-layout">
-        <Library
-          stories={stories}
-          apiToken={apiToken}
-          setSelectedStory={setSelectedStory}
-          setStories={setStories}
-        />
-        <Editor
-          apiToken={apiToken}
-          selectedStory={selectedStory}
-          setSelectedStory={setSelectedStory}
-          stories={stories}
-          setStories={setStories}
-        />
-        <AsideSettings
-          apiToken={apiToken}
-          selectedStory={selectedStory}
-          setSelectedStory={setSelectedStory}
-          stories={stories}
-          setStories={setStories}
-        />
-      </div>
-    );
-  } else {
-    return <AuthenticatePrompt setApiToken={setApiToken} />;
-  }
+  return apiToken ? (
+    <MainLayout apiToken={apiToken} />
+  ) : (
+    <AuthenticatePrompt setApiToken={setApiToken} />
+  );
 }
---
+++ src/public/component/MainLayout.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Story from "../type/storyType";
+import AsideSettings from "./AsideSettings";
+import Editor from "./Editor";
+import Library from "./Library";
+import { getStories } from "../api/storiesApi";
+
+export default function MainLayout({ apiToken }: { apiToken: string }) {
+  const [stories, setStories] = useState<Story[]>([]);
+  const [selectedStory, setSelectedStory] = useState<Story | null>({
+    id: "",
+    title: "",
+    content: "",
+  });
+
+  useEffect(() => {
+    getStories(apiToken)
+      .then((stories) => {
+        if (stories) {
+          setStories(stories);
+        }
+      })
+      .catch((error) => {
+        // probably failed because unauthroized
+        console.error("Error fetching stories:", error);
+      });
+  }, [apiToken]);
+
+  return (
+    <div className="three-column-layout">
+      <Library
+        stories={stories}
+        apiToken={apiToken}
+        setSelectedStory={setSelectedStory}
+        setStories={setStories}
+      />
+      <Editor
+        apiToken={apiToken}
+        selectedStory={selectedStory}
+        setSelectedStory={setSelectedStory}
+        stories={stories}
+        setStories={setStories}
+      />
+      <AsideSettings
+        apiToken={apiToken}
+        selectedStory={selectedStory}
+        setSelectedStory={setSelectedStory}
+        stories={stories}
+        setStories={setStories}
+      />
+    </div>
+  );
+}
