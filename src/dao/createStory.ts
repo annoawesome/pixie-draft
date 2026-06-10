aeb2e371d68a467dd1d89e8cfae1d33fd36f4ede
Seth Hoong <hoongseth@gmail.com>
2026-06-10 02:33:29 +0000

    Add history support to backend

--- src/dao/createStory.ts
+++ src/dao/createStory.ts
@@ -6,8 +6,9 @@ export default function createStory(title: string, content: string) {
 
   const id = crypto.randomUUID();
   const stories = JSON.parse(fs.readFileSync(storiesPath, "utf-8"));
-  stories.push({ id, title, content });
+  const story = { id, title, content, history: [content], historyIndex: 0 };
+  stories.push(story);
   fs.writeFileSync(storiesPath, JSON.stringify(stories, null, 2));
 
-  return { id, title, content };
+  return story;
 }
