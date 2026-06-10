6e85f9881d577f51491cb98ebc86524c4d69b46f
Seth Hoong <hoongseth@gmail.com>
2026-06-10 03:03:22 +0000

    Darken icons when buttons are disabled

--- src/public/component/Icons.tsx
+++ src/public/component/Icons.tsx
@@ -14,8 +14,8 @@ export function UndoIcon() {
       viewBox="0 0 24 24"
     >
       <path
-        fill="#e0e0e0"
         d="M13.5 7a6.5 6.5 0 0 1 6.5 6.5a6.5 6.5 0 0 1-6.5 6.5H10v-2h3.5c2.5 0 4.5-2 4.5-4.5S16 9 13.5 9H7.83l3.08 3.09L9.5 13.5L4 8l5.5-5.5l1.42 1.41L7.83 7zM6 18h2v2H6z"
+        className="path-icon"
       ></path>
     </svg>
   );
@@ -30,8 +30,8 @@ export function RedoIcon() {
       viewBox="0 0 24 24"
     >
       <path
-        fill="#e0e0e0"
         d="M10.5 7A6.5 6.5 0 0 0 4 13.5a6.5 6.5 0 0 0 6.5 6.5H14v-2h-3.5C8 18 6 16 6 13.5S8 9 10.5 9h5.67l-3.08 3.09l1.41 1.41L20 8l-5.5-5.5l-1.42 1.41L16.17 7zM18 18h-2v2h2z"
+        className="path-icon"
       ></path>
     </svg>
   );
@@ -46,8 +46,8 @@ export function RefreshIcon() {
       viewBox="0 0 24 24"
     >
       <path
-        fill="#e0e0e0"
         d="M17.65 6.35A7.96 7.96 0 0 0 12 4a8 8 0 0 0-8 8a8 8 0 0 0 8 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0 1 12 18a6 6 0 0 1-6-6a6 6 0 0 1 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4z"
+        className="path-icon"
       ></path>
     </svg>
   );
@@ -61,7 +61,7 @@ export function PaperAirplaneIcon() {
       height={24}
       viewBox="0 0 24 24"
     >
-      <path fill="#e0e0e0" d="m2 21l21-9L2 3v7l15 2l-15 2z"></path>
+      <path d="m2 21l21-9L2 3v7l15 2l-15 2z" className="path-icon"></path>
     </svg>
   );
 }
