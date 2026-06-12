101f98b8980794274c2b6e299ca454dcc9a30ca8
Seth Hoong <hoongseth@gmail.com>
2026-06-12 22:11:24 +0000

    Fix URI not being provided when endpoint profiles service gets an HTTP error

--- src/public/service/endpointProfilesService.ts
+++ src/public/service/endpointProfilesService.ts
@@ -4,20 +4,21 @@ import Endpoint from "../type/endpointType";
 
 export async function fetchUriFromEndpointProfiles(apiToken: string) {
   const settings = await getSettings(apiToken);
-
-  let uri = "http://localhost:5001";
   const endpointProfiles: Endpoint[] = settings.endpoints;
 
   for (const endpointProfile of endpointProfiles) {
-    uri = endpointProfile.uri;
+    const uri = endpointProfile.uri;
+    try {
+      const model = await fetchModel(uri);
 
-    const model = await fetchModel(uri);
-
-    if (model) {
-      console.log(`Model found via URI "${uri}":`, model);
-      break;
+      if (model) {
+        console.log(`Model found via URI "${uri}":`, model);
+        return uri;
+      }
+    } catch {
+      /* empty */
     }
   }
 
-  return uri;
+  return "http://localhost:5001";
 }
