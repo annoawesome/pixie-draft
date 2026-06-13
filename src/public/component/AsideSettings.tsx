6987a2cf0974c71c2d571ae3908d7ff4ad093cee
Seth Hoong <hoongseth@gmail.com>
2026-06-13 15:55:29 +0000

    Implement stories downloads

--- src/public/component/AsideSettings.tsx
+++ src/public/component/AsideSettings.tsx
@@ -3,6 +3,25 @@ import React from "react";
 import { deleteStory } from "../api/storiesApi";
 import Story, { removeStoryFromStories } from "../type/storyType";
 
+function downloadText(text: string, mimeType: string, fileName: string) {
+  const file = new Blob([text], {
+    type: mimeType,
+  });
+
+  const anchor = document.createElement("a");
+  const url = URL.createObjectURL(file);
+  anchor.href = url;
+  anchor.download = fileName;
+
+  document.body.append(anchor);
+  anchor.click();
+
+  setTimeout(() => {
+    document.body.removeChild(anchor);
+    URL.revokeObjectURL(url);
+  }, 0);
+}
+
 export default function AsideSettings({
   apiToken,
   selectedStory,
@@ -23,10 +42,34 @@ export default function AsideSettings({
       setSelectedStory(null);
     }
   };
+
+  const onClickExportAsText = () => {
+    downloadText(
+      selectedStory?.content ?? "",
+      "text/plain",
+      selectedStory?.title ?? "story",
+    );
+  };
+
+  const onClickExportAsJson = () => {
+    downloadText(
+      JSON.stringify(selectedStory),
+      "application/json",
+      selectedStory?.title ?? "story",
+    );
+  };
+
   return (
     <aside className="flex-column side-column" id="aside-settings">
       {selectedStory ? (
         <>
+          <button className="button-secondary" onClick={onClickExportAsText}>
+            Download as text
+          </button>
+          <button className="button-secondary" onClick={onClickExportAsJson}>
+            Download as JSON
+          </button>
+
           <button className="button-secondary" onClick={onClickDelete}>
             Delete
           </button>
