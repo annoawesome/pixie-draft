141c16920164879d77e71611b0390d9679b1c335
Seth Hoong <hoongseth@gmail.com>
2026-06-13 01:48:33 +0000

    Refactor settings into multiple subcomponents

--- src/public/component/Settings.tsx
+++ src/public/component/Settings.tsx
@@ -4,6 +4,71 @@ import EndpointProfilesSettings from "./settings/EndpointProfilesSettings";
 import { SidebarIcon, UndoIcon } from "./Icons";
 import { CurrentPage } from "../type/currentPageType";
 
+function SidebarActionsBar({
+  hideSidebar,
+  setHideSidebar,
+  setCurrentPage,
+}: {
+  hideSidebar: boolean;
+  setHideSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+  setCurrentPage: React.Dispatch<React.SetStateAction<CurrentPage>>;
+}) {
+  return (
+    <div className="flex-row width-fill-max">
+      <div className="flex-row width-fill-max">
+        <button
+          type="button"
+          className="button-tertiary button-icon"
+          title="Return to main editor"
+          hidden={hideSidebar}
+          onClick={() => setCurrentPage("main")}
+        >
+          <UndoIcon />
+        </button>
+      </div>
+      <div className="flex-row-right width-fill-max">
+        <button
+          type="button"
+          className="button-tertiary button-icon"
+          title="Hide sidebar"
+          onClick={() => setHideSidebar(!hideSidebar)}
+        >
+          <SidebarIcon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function SettingsSidebar({
+  hideSidebar,
+  setHideSidebar,
+  setCurrentPage,
+}: {
+  hideSidebar: boolean;
+  setHideSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+  setCurrentPage: React.Dispatch<React.SetStateAction<CurrentPage>>;
+}) {
+  return (
+    <aside
+      className={"flex-column sidebar" + (hideSidebar ? " sidebar-small" : "")}
+      id="settings-sidebar"
+    >
+      <SidebarActionsBar
+        hideSidebar={hideSidebar}
+        setHideSidebar={setHideSidebar}
+        setCurrentPage={setCurrentPage}
+      />
+      <button
+        className="button-tertiary button-settings-sidebar"
+        hidden={hideSidebar}
+      >
+        Endpoint Profiles
+      </button>
+    </aside>
+  );
+}
+
 export default function Settings({
   apiToken,
   setCurrentPage,
@@ -15,42 +80,11 @@ export default function Settings({
 
   return (
     <div className="flex-row" id="settings-layout">
-      <aside
-        className={
-          "flex-column sidebar" + (hideSidebar ? " sidebar-small" : "")
-        }
-        id="settings-sidebar"
-      >
-        <div className="flex-row width-fill-max">
-          <div className="flex-row width-fill-max">
-            <button
-              type="button"
-              className="button-tertiary button-icon"
-              title="Return to main editor"
-              hidden={hideSidebar}
-              onClick={() => setCurrentPage("main")}
-            >
-              <UndoIcon />
-            </button>
-          </div>
-          <div className="flex-row-right width-fill-max">
-            <button
-              type="button"
-              className="button-tertiary button-icon"
-              title="Hide sidebar"
-              onClick={() => setHideSidebar(!hideSidebar)}
-            >
-              <SidebarIcon />
-            </button>
-          </div>
-        </div>
-        <button
-          className="button-tertiary button-settings-sidebar"
-          hidden={hideSidebar}
-        >
-          Endpoint Profiles
-        </button>
-      </aside>
+      <SettingsSidebar
+        hideSidebar={hideSidebar}
+        setHideSidebar={setHideSidebar}
+        setCurrentPage={setCurrentPage}
+      />
       <main className="width-fill-max" id="">
         <EndpointProfilesSettings apiToken={apiToken} />
       </main>
