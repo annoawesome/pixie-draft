e7329d1910edda08a52c6938540e4bacbc6b8ccd
Seth Hoong <hoongseth@gmail.com>
2026-06-23 16:55:40 +0000

    Add tests for database initialization

--- test/initialization.test.js
+++ test/initialization.test.js
@@ -1,5 +1,10 @@
-import { describe, expect, test } from "vitest";
+import fs from "fs";
+
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
 import { getSecret, initializeSecret } from "../src/init/initializeSecrets.js";
+import initializeDatabase, {
+  getDatabaseFile,
+} from "../src/init/initializeDatabase.js";
 
 describe("initialization", () => {
   test("initialize secret with given secret", () => {
@@ -12,5 +17,33 @@ describe("initialization", () => {
 
     // Shouldn't be empty anyways
     expect(getSecret()).toBeTruthy();
+  });
+});
+
+describe("initialize database files", () => {
+  beforeEach(() => {
+    fs.mkdirSync("./test_environment/test/db", { recursive: true });
+  });
+
+  afterEach(() => {
+    fs.rmSync("./test_environment/test/db", { recursive: true, force: true });
+  });
+
+  test("data files exist", () => {
+    initializeDatabase("./test_environment/test/db");
+
+    expect(fs.existsSync("./test_environment/test/db/stories.json")).toBe(true);
+    expect(fs.existsSync("./test_environment/test/db/settings.json")).toBe(
+      true,
+    );
+  });
+
+  test("join paths to data files", () => {
+    initializeDatabase("./test_environment/test/db");
+
+    expect(getDatabaseFile("stories")).toBe("test_environment/test/db/stories");
+    expect(getDatabaseFile("settings")).toBe(
+      "test_environment/test/db/settings",
+    );
   });
 });
