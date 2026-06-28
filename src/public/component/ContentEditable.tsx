afe4dcc54b9c9e30f5637f6a10fb902f0acf859e
Seth Hoong <hoongseth@gmail.com>
2026-06-28 00:38:25 +0000

    Add markdown subset rendering support

--- src/public/component/ContentEditable.tsx
+++ src/public/component/ContentEditable.tsx
@@ -4,10 +4,53 @@
 
 import React, { useEffect, useRef } from "react";
 
+function wrapInHtmlElement(element: string, section: string) {
+  return `<${element}>${section}</${element}>`;
+}
+
+/**
+ * Converts a line of text into HTML, following a subset of markdown. The following features are supported:
+ *
+ * \_text\_ -> \<i>text\</i>
+ *
+ * \_\_text\_\_ -> \<b>text\</b>
+ *
+ * \_\_\_text\_\_\_ -> \<i>\<b>text\</b>\</i>
+ *
+ * \# Header 1 -> \<h1>Header 1\</h1>
+ *
+ * \## Header 1 -> \<h2>Header 1\</h2>
+ *
+ * @param section A single line
+ * @returns A single line wrapped in the appropriate HTML elements
+ */
+function convertMarkdownSectionToHtml(section: string) {
+  section = section.replaceAll(
+    /(?<![\w>])___.+?___(?![\w<])/g,
+    (match) => `<i><b>${match}</b></i>`,
+  );
+  section = section.replaceAll(
+    /(?<![\w>])__.+?__(?![\w<])/g,
+    (match) => `<b>${match}</b>`,
+  );
+  section = section.replaceAll(
+    /(?<![\w>])_.+?_(?![\w<])/g,
+    (match) => `<i>${match}</i>`,
+  );
+
+  if (section.startsWith("# ")) {
+    return wrapInHtmlElement("h1", section);
+  } else if (section.startsWith("## ")) {
+    return wrapInHtmlElement("h2", section);
+  } else {
+    return wrapInHtmlElement("p", section);
+  }
+}
+
 function splitIntoParagraphs(text: string) {
   return text
     .split("\n")
-    .map((section) => (section ? `<p>${section}</p>` : "")) // removes extra newline, sometimes not desirable and could be a bug
+    .map((section) => (section ? convertMarkdownSectionToHtml(section) : "")) // removes extra newline, sometimes not desirable and could be a bug
     .join("\n");
 }
 
