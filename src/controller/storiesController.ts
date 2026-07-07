58e4b9bc0c5f7a0bdd157bdd023766755d912570
Seth Hoong <hoongseth@gmail.com>
2026-07-07 17:47:38 +0000

    Validate imported `stories.json`

--- src/controller/storiesController.ts
+++ src/controller/storiesController.ts
@@ -7,6 +7,7 @@ import { getDatabaseFile } from "../init/initializeDatabase.js";
 import checkStoryFileExists from "../dao/stories/checkStoryFileExistsDao.js";
 import { downloadService } from "../service/downloadService.js";
 import path from "path";
+import { StoriesSchema } from "../type/storyType.js";
 
 export function getStoriesDownload(req: Request, res: Response) {
   const storiesFileExists = checkStoryFileExists();
@@ -31,7 +32,18 @@ export function postStoriesUpload(req: Request, res: Response) {
   req.pipe(writeStream);
 
   writeStream.on("finish", () => {
-    res.sendStatus(HttpStatusCodes.CREATED);
+    fs.readFile(path.resolve(getDatabaseFile(fileName)), (err, data) => {
+      const storiesString = data.toString("utf-8");
+
+      try {
+        // we don't need the data, just that it doesn't throw errors if the data matches
+        StoriesSchema.parse(JSON.parse(storiesString));
+
+        res.sendStatus(HttpStatusCodes.CREATED);
+      } catch {
+        res.sendStatus(HttpStatusCodes.BAD_REQUEST);
+      }
+    });
   });
 
   writeStream.on("error", () => {
