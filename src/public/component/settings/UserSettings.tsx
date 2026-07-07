8e5c646fc5f778089f285bfc240f49176e7f6f2b
Seth Hoong <hoongseth@gmail.com>
2026-07-07 16:03:31 +0000

    Move import stories dialog to its own component

--- src/public/component/settings/UserSettings.tsx
+++ src/public/component/settings/UserSettings.tsx
@@ -3,6 +3,43 @@ import { downloadFromUrl } from "../../service/downloadClientService";
 import { getDownloadUrl } from "../../service/userDataService";
 import Dialog from "../Dialog";
 
+function ImportStoriesDialog({
+  showImportStoriesDialog,
+  setShowImportStoriesDialog,
+}: {
+  showImportStoriesDialog: boolean;
+  setShowImportStoriesDialog: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const onClickCancelImportStories = () => setShowImportStoriesDialog(false);
+
+  return (
+    <Dialog
+      showDialog={showImportStoriesDialog}
+      setShowDialog={setShowImportStoriesDialog}
+    >
+      <form className="flex-column gap-medium">
+        <h1>Import stories?</h1>
+        <p>
+          This will erase <i>all</i> of your currently saved stories!
+        </p>
+        <input type="file" name="" id="" accept="application/json" />
+        <div className="flex-row gap-small">
+          <button type="button" className="button-secondary">
+            Yes
+          </button>
+          <button
+            type="button"
+            className="button-secondary"
+            onClick={onClickCancelImportStories}
+          >
+            No
+          </button>
+        </div>
+      </form>
+    </Dialog>
+  );
+}
+
 export function UserSettings() {
   const [showImportStoriesDialog, setShowImportStoriesDialog] = useState(false);
 
@@ -18,8 +55,6 @@ export function UserSettings() {
   const onClickImportStories = () => {
     setShowImportStoriesDialog(true);
   };
-
-  const onClickCancelImportStories = () => setShowImportStoriesDialog(false);
 
   return (
     <div className="flex-column settings-section gap-medium" id="user-section">
@@ -39,30 +74,10 @@ export function UserSettings() {
         >
           Import stories
         </button>
-        <Dialog
-          showDialog={showImportStoriesDialog}
-          setShowDialog={setShowImportStoriesDialog}
-        >
-          <form className="flex-column gap-medium">
-            <h1>Import stories?</h1>
-            <p>
-              This will erase <i>all</i> of your currently saved stories!
-            </p>
-            <input type="file" name="" id="" accept="application/json" />
-            <div className="flex-row gap-small">
-              <button type="button" className="button-secondary">
-                Yes
-              </button>
-              <button
-                type="button"
-                className="button-secondary"
-                onClick={onClickCancelImportStories}
-              >
-                No
-              </button>
-            </div>
-          </form>
-        </Dialog>
+        <ImportStoriesDialog
+          showImportStoriesDialog={showImportStoriesDialog}
+          setShowImportStoriesDialog={setShowImportStoriesDialog}
+        />
       </div>
     </div>
   );
