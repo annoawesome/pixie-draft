307fc208f96f96e4128fb634c9ba38390969835c
Seth Hoong <hoongseth@gmail.com>
2026-07-22 03:12:43 +0000

    Use Zod for KoboldCpp generation output validation

--- src/public/client/koboldCppClient.ts
+++ src/public/client/koboldCppClient.ts
@@ -1,30 +1,15 @@
+import z from "zod";
+
 import { getModel, postGenerate } from "../api/koboldCppApi";
 import { LlmEndpointClient } from "../type/llmEndpointClient";
 
-interface GenerationResult {
-  text: string;
-}
-
-interface GenerationOutput {
-  results: GenerationResult[];
-}
-
-function isGenerationResult(result): result is GenerationResult {
-  return typeof result === "object" && typeof result.text === "string";
-}
-
-/**
- * Tests whether a response is a generation output.
- * @param response A JSON object returned by the /generate endpoint
- * @returns Whether the return object is of type `GenerationOutput`
- */
-function isGenerationOutput(response): response is GenerationOutput {
-  return (
-    typeof response === "object" &&
-    Array.isArray(response.results) &&
-    response.results.every(isGenerationResult)
-  );
-}
+const GenerationOutputSchema = z.object({
+  results: z.array(
+    z.object({
+      text: z.string(),
+    }),
+  ),
+});
 
 export class KoboldCppClient implements LlmEndpointClient {
   #baseUrl: string;
@@ -41,14 +26,17 @@ export class KoboldCppClient implements LlmEndpointClient {
       prompt,
       this.#authorization,
     );
-    const output = await response.json();
 
-    if (isGenerationOutput(output) && output.results.length > 0) {
-      const result = output.results[0];
+    const generationOutput = GenerationOutputSchema.parse(
+      await response.json(),
+    );
+
+    if (generationOutput.results.length > 0) {
+      const result = generationOutput.results[0];
       return result.text;
     }
 
-    throw new Error("Did not respond with generation output:", output);
+    throw new Error("Did not respond with generation results");
   }
 
   async fetchModel(): Promise<string> {
