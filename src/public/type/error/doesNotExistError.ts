5d3694f4115a54ae917ece51c221e8ba2b108b98
Seth Hoong <hoongseth@gmail.com>
2026-07-30 15:26:25 +0000

    Add name property to `DoesNotExistError` class

--- src/public/type/error/doesNotExistError.ts
+++ src/public/type/error/doesNotExistError.ts
@@ -1,4 +1,6 @@
 /**
  * Thrown if an interface or method requires that something exists yet is unable to find it.
  */
-export default class DoesNotExistError extends Error {}
+export default class DoesNotExistError extends Error {
+  name = "DoesNotExistError";
+}
