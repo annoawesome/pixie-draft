dec2cc6570423724486f276db185d8750995ab01
Seth Hoong <hoongseth@gmail.com>
2026-08-15 01:38:37 +0000

    Add documentation for `DocumentTitleRef` component

--- src/public/component/DocumentTitleRef.tsx
+++ src/public/component/DocumentTitleRef.tsx
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 
+/**
+ * Does not actually return a populated React node,
+ * but does update the document title to match the state.
+ */
 export default function DocumentTitleRef({ title }: { title: string }) {
   useEffect(() => {
     document.title = title;
