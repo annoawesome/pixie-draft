16a66a4c97d61708aeb84e71c6f3d4c1843f5df6
Seth Hoong <hoongseth@gmail.com>
2026-08-15 01:33:53 +0000

    Add `DocumentTitleRef` component to update document title

---
+++ src/public/component/DocumentTitleRef.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function DocumentTitleRef({ title }: { title: string }) {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  return <></>;
+}
